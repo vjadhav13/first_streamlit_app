b749700440061a11ca4d665708f6d1b08799f4a0
Vijay Jadhav <48402413+vjadhav13@users.noreply.github.com>
2022-08-19 14:21:50 +0530

    Update streamlit_app.py
    Explicitly added error to the code

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
+strealit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
